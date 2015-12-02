4585c15c71ef8b0a2f8e81178c1576936797b72b
chemonky <luricardorod@gmail.com>
2015-12-02 16:42:27 -0600

    :rocket: wear the girl in shop whith elements miissing setup sprites

--- js/preload.js
+++ js/preload.js
@@ -19,8 +19,63 @@ Preload.prototype = {
 		this.game.load.image('scores','assets/my_score.png');
 		this.game.load.audio('music', 'assets/music.mp3');
 
-
-
+		//this adds shop images
+		this.game.load.image('dollUnderware', 'assets/maniqui/maniqui.png');
+		//dress
+		this.game.load.image('buttonDress', 'assets/vastido1.png');
+		this.game.load.image('dress1', 'assets/vestido/vestido1.png');
+		this.game.load.image('dress2', 'assets/vestido/vestido2.png');
+		this.game.load.image('dress3', 'assets/vestido/vestido3.png');
+		this.game.load.image('dress4', 'assets/vestido/vestido4.png');
+		this.game.load.image('maniquiDress1', 'assets/maniquivestido/maniquivestido1.png');
+		this.game.load.image('maniquiDress2', 'assets/maniquivestido/maniquivestido2.png');
+		this.game.load.image('maniquiDress3', 'assets/maniquivestido/maniquivestido3.png');
+		this.game.load.image('maniquiDress4', 'assets/maniquivestido/maniquivestido4.png');
+		//shopSkirt
+		this.game.load.image('buttonSkirt', 'assets/vastido1.png');
+		this.game.load.image('skirt1', 'assets/falda/falda1.png');
+		this.game.load.image('skirt2', 'assets/falda/falda2.png');
+		this.game.load.image('skirt3', 'assets/falda/falda3.png');
+		this.game.load.image('skirt4', 'assets/falda/falda4.png');
+		this.game.load.image('maniquiSkirt1', 'assets/maniquifalda/maniquifalda1.png');
+		this.game.load.image('maniquiSkirt2', 'assets/maniquifalda/maniquifalda2.png');
+		this.game.load.image('maniquiSkirt3', 'assets/maniquifalda/maniquifalda3.png');
+		this.game.load.image('maniquiSkirt4', 'assets/maniquifalda/maniquifalda4.png');
+		//shopBag
+		this.game.load.image('buttonBag', 'assets/vastido1.png');
+		this.game.load.image('bag1', 'assets/bolsa/bolsa4.png');
+		this.game.load.image('bag2', 'assets/bolsa/bolsa3.png');
+		this.game.load.image('bag3', 'assets/bolsa/bolsa2.png');
+		this.game.load.image('bag4', 'assets/bolsa/bolsa1.png');
+		this.game.load.image('maniquiBag1', 'assets/maniquiBolsas/maniquiBolsa1.png');
+		this.game.load.image('maniquiBag2', 'assets/maniquiBolsas/maniquiBolsa2.png');
+		this.game.load.image('maniquiBag3', 'assets/maniquiBolsas/maniquiBolsa3.png');
+		this.game.load.image('maniquiBag4', 'assets/maniquiBolsas/maniquiBolsa4.png');
+		//shopHair
+		this.game.load.image('buttonHair', 'assets/vastido1.png');
+		this.game.load.image('hair1', 'assets/cabello/1/cabello1back.png');
+		this.game.load.image('hair2', 'assets/cabello/1/cabello1front.png');
+		this.game.load.image('hair3', 'assets/cabello/2/cabello2back.png');
+		this.game.load.image('hair4', 'assets/cabello/2/cabello2front.png');
+		this.game.load.image('hair6', 'assets/cabello/3/cabello3.png');
+		this.game.load.image('hair7', 'assets/cabello/4/cabello4back.png');
+		this.game.load.image('hair8', 'assets/cabello/4/cabello4front.png');
+		this.game.load.image('maniquiHair1', 'assets/maniquicabello/maniquipelo1.png');
+		this.game.load.image('maniquiHair2', 'assets/maniquicabello/maniquipelo2.png');
+		this.game.load.image('maniquiHair3', 'assets/maniquicabello/maniquipelo3.png');
+		this.game.load.image('maniquiHair4', 'assets/maniquicabello/maniquipelo4.png');
+		//hood
+		this.game.load.image('buttonHood', 'assets/vastido1.png');
+		this.game.load.image('hood1', 'assets/capa/capa1/capa1back.png');
+		this.game.load.image('hood2', 'assets/capa/capa1/capa1front.png');
+		this.game.load.image('hood3', 'assets/capa/capa2/capa2back.png');
+		this.game.load.image('hood4', 'assets/capa/capa2/capa2front.png');
+		this.game.load.image('hood5', 'assets/capa/capa3/capa3.png');
+		this.game.load.image('hood7', 'assets/capa/capa4/capa4.png');
+		this.game.load.image('maniquiHood1', 'assets/maniquicapa/maniquicapa1.png');
+		this.game.load.image('maniquiHood2', 'assets/maniquicapa/maniquicapa2.png');
+		this.game.load.image('maniquiHood3', 'assets/maniquicapa/maniquicapa3.png');
+		this.game.load.image('maniquiHood4', 'assets/maniquicapa/maniquicapa4.png');
 		// This adds Buttons
 		this.game.load.image('shop_btn', 'assets/shop_btn.png');
 		this.game.load.image('play_btn', 'assets/play_btn.png');
